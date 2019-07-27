6f52669d3d1de8c87a7deeed1804d117a97c2e08
loki218 <ergash218@live.ru>
2019-07-28 00:42:37 +0500

    omg

--- startUp/settings.py
+++ startUp/settings.py
@@ -3,7 +3,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '02_*f8gyvmk+m7xq2+-t3%ywy7jp3(-x^qr3_lw6!d4!rz0*^)'
 
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'startUp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
