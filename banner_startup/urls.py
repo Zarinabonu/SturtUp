72370d93181b5797a2a1e183b3972a51bcd813fe
Zarinabonu <zarinabonu199924@gmail.com>
2019-07-29 09:39:31 +0500

    a

--- banner_startup/urls.py
+++ banner_startup/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from banner_startup import views
 
 urlpatterns = [
-    path('list/',views.BannerListView.as_view(), name ='banner-list'),
-    path('create/', views.BannerCreteView.as_view(), name ='banner-create'),
+    path('',views.BannerListView.as_view(), name ='banner-list'),
+    # path('create/', views.BannerCreteView.as_view(), name ='banner-create'),
     path('update/<int:pk>', views.BannerUpdateView.as_view(), name ='banner-update'),
     path('', views.index, name ='main'),
     
